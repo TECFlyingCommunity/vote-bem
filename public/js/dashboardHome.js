835afd0ebdbabbae45068b0b282962b5d7522416
victor <victor__carvalhodesa@hotmail.com>
2021-09-16 01:17:09 -0300

    bug interface fix

---
+++ public/js/dashboardHome.js
@@ -0,0 +1,24 @@
+var modal = document.getElementById("box");
+
+
+var btn = document.getElementById("btn_pop");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
